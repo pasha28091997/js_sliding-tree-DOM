d831645e3bc73423bac314ccb897bda1ff3cadba
Pasha Shchukin <pasha4320692@gmail.com>
2024-11-03 17:21:10 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write code here
+document.querySelectorAll('.tree > li').forEach((node) => {
+  wrapTextInSpan(node);
+});
+
+function wrapTextInSpan(node) {
+  const span = document.createElement('span');
+
+  span.textContent = node.childNodes[0].textContent.trim();
+  node.childNodes[0].replaceWith(span);
+
+  span.addEventListener('click', () => {
+    toggleSubtree(node);
+  });
+}
+
+function toggleSubtree(node) {
+  const subtree = node.querySelector('ul');
+
+  if (subtree) {
+    subtree.hidden = !subtree.hidden;
+  }
+}
